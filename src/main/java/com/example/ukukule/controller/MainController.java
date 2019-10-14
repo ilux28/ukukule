9a2e897e29e877f5aba39a284d02332251ff94da
ilux28 <ilya.pribytko@mail.ru>
2019-10-14 22:20:13 +0500

    uKuKule project(Added authorization and authentication)

--- src/main/java/com/example/ukukule/controller/MainController.java
+++ src/main/java/com/example/ukukule/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.example.ukukule;
+package com.example.ukukule.controller;
 
 import com.example.ukukule.domain.Message;
 import com.example.ukukule.repo.MessageRepo;
@@ -12,27 +12,23 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-public class GrettingController {
+public class MainController {
 
     @Autowired
     private MessageRepo messageRepo;
 
-    @GetMapping("/greeting")
-    public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World")  String name,
-       Map<String, Object> model
-    ) {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting( Map<String, Object> model ) {
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
         return "main";
     }
-    @PostMapping
+    @PostMapping("/main")
     public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
         Message message = new Message(text, tag);
         messageRepo.save(message);
---
+++ src/main/java/com/example/ukukule/repo/UserRepo.java
@@ -0,0 +1,9 @@
+package com.example.ukukule.repo;
+
+import com.example.ukukule.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
